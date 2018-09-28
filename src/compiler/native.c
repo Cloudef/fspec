b0ac4a02ad7bb4af546fa070191b099bdc9e3581
Jari Vetoniemi <mailroxas@gmail.com>
2018-09-28 14:13:11 +0300

    types: handle raw string bytes

--- src/compiler/native.c
+++ src/compiler/native.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <limits.h>
 #include <assert.h>
+#include <err.h>
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
 static inline void*
 ptr_from_value(value_t ptr)
@@ -94,6 +97,46 @@ c_strtoull(program_t *prg, tree_t **sp, str_t *a, value_t b)
    memcpy(buf, a->value->data, a->value->length);
    colm_tree_downref(prg, sp, (tree_t*)a);
    return strtoull(buf, NULL, b);
+}
+
+str_t*
+c_esc2chr(program_t *prg, tree_t **sp, str_t *a)
+{
+   assert(a);
+
+   static const struct { const char e, v; } map[] = {
+      { .e = 'a', .v = '\a' },
+      { .e = 'b', .v = '\b' },
+      { .e = 'f', .v = '\f' },
+      { .e = 'n', .v = '\n' },
+      { .e = 'r', .v = '\r' },
+      { .e = 't', .v = '\t' },
+      { .e = 'v', .v = '\v' },
+      { .e = '\\', .v = '\\' },
+      { .e = '\'', .v = '\'' },
+      { .e = '\"', .v = '"' },
+      { .e = 'e', .v = 0x1B },
+   };
+
+   for (size_t i = 0; i < ARRAY_SIZE(map); ++i) {
+      if (*a->value->data != map[i].e)
+         continue;
+
+      tree_t *s = construct_string(prg, colm_string_alloc_pointer(prg, &map[i].v, 1));
+      return (str_t*)upref(prg, s);
+   }
+
+   errx(EXIT_FAILURE, "%s: unknown escape character `%c`", __func__, *a->value->data);
+   return NULL;
+}
+
+str_t*
+c_num2chr(program_t *prg, tree_t **sp, value_t a)
+{
+   static const uint8_t u8[256] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255 };
+   assert(a < ARRAY_SIZE(u8));
+   tree_t *s = construct_string(prg, colm_string_alloc_pointer(prg, &u8[(uint8_t)a], 1));
+   return (str_t*)upref(prg, s);
 }
 
 value_t
